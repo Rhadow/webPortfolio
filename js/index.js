2cca06f089683cb88aa1fdf34f187c553d774847
Rhadow <howard3433@gmail.com>
2014-10-01 22:23:25 +0800

    added wobble effect on about bit circle links and modify the section h1 & p styling

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(function() {
             var target = $(this.hash);
             if (target) {
                 $("html, body").animate({
-                	//Scroll to target's top minus the header's height
+                    //Scroll to target's top minus the header's height
                     scrollTop: target.offset().top - $("header").outerHeight()
                 }, 1000);
             }
@@ -13,10 +13,33 @@ $(function() {
         }
     });
 
+    /* Make the big circle links in the about section to wiggle(nod) when hover */
+    $("a[class*=moreLink]").on("mouseenter", function() {
+        var freq = 5;
+        var animationTime = 50;
+        var intensity = 5;
+        var self = $(this);
+        var shake = setInterval(function() {
+            if (freq <= 0) {
+                clearInterval(shake);
+            }
+            if (freq % 2 !== 0) {
+                self.animate({
+                    top: "+=" + intensity
+                }, animationTime);
+            } else {
+                self.animate({
+                    top: "-=" + intensity
+                }, animationTime);
+            }
+            --freq;
+        }, animationTime);
+    });
+
     /* Tracks the scroll position and create effects */
     $(window).on("scroll", function() {
         var curPos = $(window).scrollTop();
-        if (curPos > $("#about").outerHeight() / 2) {
+        if (curPos > $("#about").outerHeight() / 1.5) {
             $("header").slideDown("1000");
         } else {
             $("header").slideUp("1000");
